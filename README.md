2cbf32ed2a85a47e4e6ec25bb0f23c3188cd91c3
DevRoque <dev.roque95@gmail.com>
2024-05-09 12:41:58 -0300

    Projeto Agenda

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# django_agenda
+# DJANGO AGENDA
+
+## Iniciar o projeto Django
+
+Crie o ambiente virtual para começar o seu projeto isso te ajudará no futuro acredite.
+```
+python -m venv venv
+. venv/bin/activate
+pip install django
+django-admin startproject project .
+python manage.py startapp contact
+```
+---
+## CONFIGURANDO O GIT
+```
+git config --global user.name 'Seu nome'
+git config --global user.email 'seu_email@gmail.com'
+git config --global init.defaultBranch main
+```
+---
+## ASSOCIANDO O GIT AO GITHUB
+
+Selecione a pasta do projeto para executar os comandos asseguir.
+```
+git init
+git add .
+git commit -m 'Mensagem'
+git remote add origin URL_DO_GIT
+```
+---
+## Migrando a base de dados do Django
+```
+python manage.py makemigrations
+python manage.py migrate
+```
+---
+## Criando e modificando a senha de um super usuário Django
+python manage.py createsuperuser
+python manage.py changepassword USERNAME
--- projeto_agenda/settings.py
+++ projeto_agenda/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'projeto_agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +110,7 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -117,8 +119,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'base_static',
+)
+STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from projeto_agenda.local_settings import *
+except ImportError:
+    ...
